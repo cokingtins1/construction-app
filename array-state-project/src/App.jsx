5564838a9a9bee32737019e49a79f3a72ab0e48c
cokingtins1 <106932061+cokingtins1@users.noreply.github.com>
2023-11-09 13:49:17 -0500

    final commit

--- array-state-project/src/App.jsx
+++ array-state-project/src/App.jsx
@@ -1,35 +1,100 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
+
+const INITIAL_VALUE = ["A", "B", "C"]
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [array, setArray] = useState(INITIAL_VALUE)
+	const [value, setValue] = useState("")
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	function removeFirstElement() {
+		setArray((currentArray) => {
+			return currentArray.slice(1)
+		})
+	}
+
+	function removeSpecificLetter(letter) {
+		setArray((currentArray) => {
+			// .filter only keeps the elements where its () evaluates to true
+			return currentArray.filter((element) => element !== letter)
+		})
+	}
+
+	function addLettertoStart(letter) {
+		setArray((currentArray) => {
+			return [letter, ...currentArray]
+		})
+	}
+
+	function addLettertoEnd(letter) {
+		setArray((currentArray) => {
+			return [...currentArray, letter]
+		})
+	}
+
+	function clear() {
+		setArray([])
+	}
+
+	function reset() {
+		setArray(INITIAL_VALUE)
+	}
+
+	function updateAtoH() {
+		setArray((currentArray) => {
+			return currentArray.map((element) =>
+				element === "A" ? "H" : element
+			)
+		})
+	}
+
+	function addLetterAtIndex(letter, index) {
+		setArray((currentArray) => {
+			return [
+				...currentArray.slice(0, index),
+				letter,
+				...currentArray.slice(index),
+			]
+		})
+	}
+
+	return (
+		<div>
+			<button onClick={removeFirstElement}>Remove First Element</button>
+			<br />
+			<button onClick={() => removeSpecificLetter("B")}>
+				Remove All B's
+			</button>
+			<br />
+			<button onClick={() => addLettertoStart("B")}>Add to Start</button>
+			<br />
+			<button onClick={() => addLettertoEnd("Z")}>Add to End</button>
+			<br />
+			<button onClick={clear}>Clear</button>
+			<br />
+			<button onClick={reset}>Reset</button>
+			<br />
+			<button onClick={updateAtoH}>Update A to H</button>
+			<br />
+			<button
+				onClick={() => {
+					addLetterAtIndex("C", 2)
+				}}
+			>
+				Add C at 2
+			</button>
+			<br />
+			<input value={value} onChange={(e) => setValue(e.target.value)} />
+			<br />
+			<button
+				onClick={() => {
+					addLettertoStart(value)
+				}}
+			>
+				Add Value to Array
+			</button>
+			{array.join(",")}
+		</div>
+	)
 }
 
 export default App
